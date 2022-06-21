b6b81d0c5b2a95fe618b4d0a3cb03fd1149089c3
Jason <jasondong03@gmail.com>
2022-06-21 13:02:01 -0500

    Creating and finishing error message

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./ErrorModal.css";
+import Card from './Card.js';
+import Button from './Button';
+
+const ErrorModal = props => {
+    return (
+        <div>
+            <div className="backdrop" onClick={props.onHandleError}/>
+            <div className="modal">
+                <Card>
+                    <header className="heade"r>
+                        <h2>{props.title}</h2>
+                    </header>
+                    <div className="content">
+                        <p>{props.message}</p>
+                    </div>
+                    <footer className="actions">
+                        <Button clickHandler={props.onHandleError}>Okay</Button>
+                    </footer>
+                </Card>
+            </div>
+        </div>
+    );
+}
+
+export default ErrorModal
--- src/components/users/UsersList.js
+++ src/components/users/UsersList.js
@@ -7,8 +7,9 @@ const UsersList = (props) => {
         <Card>
             <ul className="users">
             {props.users.map (user => 
-                <li key={user.id}>{user.name} ({user.age} years old)</li>
-            )};
+                <li key={user.id}>
+                    {user.name} ({user.age} years old)</li>
+            )}
             </ul>   
         </Card>
     );
