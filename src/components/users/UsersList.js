bdc6a290d93a1c2b3709ffb150cc3c6c9d40cf63
Sirat Mokha <mokha1671@gmail.com>
2022-06-21 11:21:23 -0500

    Creating users list

---
+++ src/components/users/UsersList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './UsersList.css'; 
+import Card from "../UI/Card";
+
+const UsersList = (props) => {
+
+    return(
+
+        <Card>
+        <ul className="users">
+
+        {props.users.map(user=>
+            <li>{user.name}({user.age} years old)</li>
+
+        )}
+
+        </ul>
+
+        </Card>     
+    ); 
+}
+
+export default UsersList;                  
