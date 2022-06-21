45fb6c42b9c28584b7ed77d4834843e553352933
Kevin Niu <kevin.h.niu@gmail.com>
2022-06-21 10:45:47 -0500

    Button styling component

---
+++ src/components/UI/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.css';
+
+const Button = props => {
+
+    return(
+        <button type={props.type || "button"} onClick={props.clickHandler}>{props.children}</button>
+    );
+
+}
+
+export default Button;
