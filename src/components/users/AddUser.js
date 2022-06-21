e3129a1b56fdd6eaf16774d66f17e5eacfef0c58
Holly R <hraetz14@gmail.com>
2022-06-21 10:11:07 -0500

    Created AddUser

---
+++ src/components/users/AddUser.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './AddUser.css';
+
+const AddUser = props => {
+
+    function addUserHandler(event){
+        event.preventDefault();
+    }
+
+    return(
+        <form onSubmit={addUserHandler}>
+            <label htmlFor='username'>Username</label>
+            <input id='username' type='text'></input>
+
+            <label htmlFor='age'>Age</label>
+            <input id='age' type='number'></input>
+
+            <button type='submit'>Add User</button>
+        </form>
+    );
+
+};
+
+export default AddUser;
