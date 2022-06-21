cda925f0f9ed5273927321a9d7f6f024d81a820a
Sirat Mokha <mokha1671@gmail.com>
2022-06-21 10:11:15 -0500

    Creating add user

---
+++ src/components/users/AddUsers.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './AddUsers.css'; 
+
+const AddUser = props => {
+
+    function addUserHandler(event){
+        event.preventDefault(); 
+    }
+
+    return(
+        <form onSubmit={addUserHandler}>
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text"></input>
+            <label htmlFor="age">Age (Years)</label>
+            <input id="age" type="number"></input>
+            <button type="sumbit">Add User</button>
+        </form>
+
+    ); 
+
+};
+
+export default AddUser;  
