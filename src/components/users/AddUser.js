f3fe4962ee6a0dad1cbcb21e792b54eaac83717d
Holly R <hraetz14@gmail.com>
2022-06-21 10:25:37 -0500

    Styling with cards

--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import './AddUser.css';
+import Card from '../UI/Card.js';
 
-const AddUser = props => {
+const AddUser = (props) => {
 
     function addUserHandler(event){
         event.preventDefault();
     }
 
     return(
-        <form onSubmit={addUserHandler}>
-            <label htmlFor='username'>Username</label>
-            <input id='username' type='text'></input>
+        <Card>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor='username'>Username</label>
+                <input id='username' type='text'></input>
 
-            <label htmlFor='age'>Age</label>
-            <input id='age' type='number'></input>
+                <label htmlFor='age'>Age</label>
+                <input id='age' type='number'></input>
 
-            <button type='submit'>Add User</button>
-        </form>
+                <button type='submit'>Add User</button>
+            </form>
+        </Card>
     );
 
 };
